3d70fcd2f9324843c155d1ca55532c567bdbc898
luisfelipesena <luisfelipesena@gmail.com>
2023-09-24 11:11:25 -0300

    add a

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,7 +2,7 @@ import type { NextRequest } from 'next/server';
 import { NextResponse } from 'next/server';
 
 export function middleware(request: NextRequest) {
-  console.log('AAAAAAAAAA');
+  console.log('AAAAAAAAAAA');
 
   if (request.nextUrl.pathname.startsWith('/teste-middleware')) {
     return NextResponse.redirect(new URL('/teste-middleware-2', request.url));
