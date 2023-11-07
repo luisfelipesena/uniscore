aa5f73d0ecc4170d8a68acf93f6d46288b20c6cc
João Leahy <joaovictorleahy.hrzon@gmail.com>
2023-11-07 18:45:39 -0300

    feat(database/db): adiciona banco de dados de usuários

---
+++ src/app/api/database/db.ts
@@ -0,0 +1,9 @@
+const { Pool } = require('pg');
+
+export const pool = new Pool({
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_DATABASE,
+  password: process.env.DB_PASSWORD,
+  port: 5432
+});
