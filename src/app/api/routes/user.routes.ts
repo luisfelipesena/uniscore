eef2c6b9cef3b1095c8e649682e1418124cd5896
João Leahy <joaovictorleahy.hrzon@gmail.com>
2023-11-07 19:37:29 -0300

    feat(routes/user.routes): implementa rotas de alteração de senha do usuário

--- src/app/api/routes/user.routes.ts
+++ src/app/api/routes/user.routes.ts
@@ -1,11 +1,12 @@
 import express from 'express';
-import { registrarUsuario, loginUsuario } from '../controllers/user.controller';
+import { registrarUsuario, loginUsuario, esqueciMinhaSenha } from '../controllers/user.controller';
 import { authMiddleware } from '../middlewares/auth.middleware';
 
 const router = express.Router();
 
 router.post('/registro', registrarUsuario);
 router.post('/login', loginUsuario);
+router.post('/esqueci-minha-senha', esqueciMinhaSenha);
 
 router.use(authMiddleware);
 
